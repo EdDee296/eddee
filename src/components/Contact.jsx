d7a7cb99a18b975c63b90a1793ea5036b7e1e74e
EdDee296 <109675085+EdDee296@users.noreply.github.com>
2023-09-10 20:29:16 -0600

    adding dist

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -77,7 +77,7 @@ const Contact = () => {
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className='flex-[0.75] bg-gradient-to-r from-orange-900 to-black-200 p-8 rounded-2xl'
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
